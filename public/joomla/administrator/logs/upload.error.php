8fbac745ac5de484b3bfec2ed2c098cc05768948
Eugene Nam jc463628 <54092140+jc463628@users.noreply.github.com>
2021-03-14 14:25:03 +1000

    Commit

---
+++ public/joomla/administrator/logs/upload.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-12 12:24:58 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-12T12:24:58+00:00	INFO 192.168.33.1	upload	
